7f1a9b890b273db06d87dbb4800a24ac95b12cb7
Ryan Kes <ryan@ryankes.eu>
2024-05-17 18:16:56 +0200

    chore: initial commit

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -14,8 +14,9 @@ import (
 // acceptance testing. The factory function will be invoked for every Terraform
 // CLI command executed to create a provider server to which the CLI can
 // reattach.
+
 var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
-	"scaffolding": providerserver.NewProtocol6WithError(New("test")()),
+	"exampletime": providerserver.NewProtocol6WithError(New("test")()),
 }
 
 func testAccPreCheck(t *testing.T) {
--- main.go
+++ main.go
@@ -6,21 +6,20 @@ package main
 import (
 	"context"
 	"flag"
-	"log"
-
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/hashicorp/terraform-provider-scaffolding-framework/internal/provider"
+	"log"
+	"terraform-provider-exampletime/internal/provider"
 )
 
 // Run "go generate" to format example terraform files and generate the docs for the registry/website
 
-// If you do not have terraform installed, you can remove the formatting command, but its suggested to
+// If you do not have terraform installed, you can remove the formatting command, but it's suggested to
 // ensure the documentation is formatted properly.
 //go:generate terraform fmt -recursive ./examples/
 
 // Run the docs generation tool, check its repository for more information on how it works and how docs
 // can be customized.
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate -provider-name scaffolding
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate -provider-name exampletime
 
 var (
 	// these will be set by the goreleaser configuration
@@ -38,10 +37,12 @@ func main() {
 	flag.Parse()
 
 	opts := providerserver.ServeOpts{
-		// TODO: Update this string with the published name of your provider.
-		// Also update the tfplugindocs generate command to either remove the
-		// -provider-name flag or set its value to the updated provider name.
-		Address: "registry.terraform.io/hashicorp/scaffolding",
+		// NOTE: This is not a typical Terraform Registry provider address, such
+		// as registry.terraform.io/hashicorp/exampletime. This specific
+		// provider address is used in this tutorial in conjunction with a
+		// specific Terraform CLI configuration for manual development testing
+		// of this provider.
+		Address: "hashicorp.com/edu/exampletime",
 		Debug:   debug,
 	}
 
